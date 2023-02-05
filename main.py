117e6266c69bf7a1066ef7b109f342945651f901
Adrian Metallinos <39782071+I-code-stuff631@users.noreply.github.com>
2023-02-05 15:31:00 -0500

    Start on Tile class and add Direction enum
    Also start using Tile class and after test increase square size from 106.25 to 107

--- main.py
+++ main.py
@@ -21,6 +21,49 @@
 #     pygame.display.flip()
 import pygame
 from pygame.locals import *  # The "Prelude"
+from enum import Enum, auto
+import random as rand
+
+
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
+class Tile:
+    """2048 Tile"""
+    @classmethod
+    def init(cls, stop_positions: list[list], border_radius):
+        """Run this before actually creating an instance of the class"""
+        cls.STOP_POSITIONS = stop_positions
+        cls.border_radius = border_radius
+
+    def __init__(self, rect):
+        self._number = 2
+        self.moving = None
+        self.rect = rect
+
+    def push(self, direction: Direction):
+        self.moving = direction
+
+    def move(self):
+        match self.moving:
+            case Direction.UP:
+                pass
+            case Direction.DOWN:
+                pass
+            case Direction.LEFT:
+                pass
+            case Direction.RIGHT:
+                pass
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, Color(0, 0, 0), self.rect, border_radius=self.border_radius)
+
+    # def increment_number(self):
+    #     self._number *= 2
 
 
 def main(screen_size):  # Treat screen_size as final unless you MUST do otherwise (trust me)
@@ -42,7 +85,7 @@ def main(screen_size):  # Treat screen_size as final unless you MUST do otherwis
     # Their grid margin is the same as the distance between their squares
     grid_margin = distance_between_squares = scaling_factor * 15  # Their grid margin
     grid_margin += big_square_margin
-    square_size = scaling_factor * 106.25  # Their (technically background) square size
+    square_size = scaling_factor * 107
     square_distance = distance_between_squares + square_size
     their_square_border_radius = 3  # For their background and forground squares
     square_border_radius = round(scaling_factor * their_square_border_radius)
@@ -51,13 +94,23 @@ def main(screen_size):  # Treat screen_size as final unless you MUST do otherwis
         [None, None, None, None],
         [None, None, None, None],
         [None, None, None, None]
-    ]  # Also doubles as the valid stop positions
+    ]  # Also doubles as the valid stop positions (that is why it is not just a list)
     for x in range(4):
         for y in range(4):
             background_square_rects[x][y] =\
                 Rect(x * square_distance + grid_margin,
                      y * square_distance + grid_margin,
                      square_size, square_size)
+    Tile.init(background_square_rects, square_border_radius)
+    tiles: list[list[Tile | None]] = [
+        [None, None, None, None],
+        [None, None, None, None],
+        [None, None, None, None],
+        [None, None, None, None]
+    ]
+    for _ in range(2):
+        x, y = rand.randrange(4), rand.randrange(4)
+        tiles[x][y] = Tile(background_square_rects[x][y].copy())
     while True:
         pygame.draw.rect(screen, Color("#bbada0"), big_square_rect, border_radius=big_square_border_radius)
         for x in background_square_rects:
