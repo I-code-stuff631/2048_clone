f87c24d056d9bc42e79d77aff887793017a0a73e
Adrian Metallinos <39782071+I-code-stuff631@users.noreply.github.com>
2023-02-04 20:56:56 -0500

    Working square grid

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def main(screen_size):  # Treat screen_size as final unless you MUST do otherwis
     screen: pygame.Surface = pygame.display.set_mode((screen_size, screen_size))
     screen.fill(Color("#faf8ef"))
     ###
-    margin = 10  # For config
+    margin = 10  # For config  # May rename main_background_square to big_square (This margin needs to be renemed anyway and big_square_margin is shorter
+    # than main_background_square_margin, also lots of other varibles names would be able to be shorter as well (I should do this next commit))
     main_background_square_rect = Rect(margin, margin, screen_size - margin * 2, screen_size - margin * 2)
     their_border_radius, their_max_border_radius = 6, 250  # Their "ratio"
     my_max_border_radius = main_background_square_rect.height / 2
@@ -37,14 +38,24 @@ def main(screen_size):  # Treat screen_size as final unless you MUST do otherwis
     my_border_radius = round(their_border_radius * (my_max_border_radius / their_max_border_radius))
     while True:
         pygame.draw.rect(screen, Color("#bbada0"), main_background_square_rect, border_radius=my_border_radius)
-        ###
-        background_square_top_left_corners = []
-        # for x in range(4):
-        #     for y in range(4):
-        #         # offset * 4 + size
-        #         background_square_top_left_corners.append(
-        #             pygame.draw.rect(screen, Color("#cdc1b4"), Rect(x, y, ))
-        #         )
+        background_square_positions = []  # Top left corners
+        for x in range(4):
+            for y in range(4):
+                their_grid_margin, their_main_background_squares_size = 15, their_max_border_radius * 2  # 500
+                my_main_background_squares_size = main_background_square_rect.height
+                scaling_factor = (my_main_background_squares_size / their_main_background_squares_size)
+                assert their_main_background_squares_size * scaling_factor == my_main_background_squares_size
+                # Their grid margin is the same as the distance between their squares
+                grid_margin_and_distance_between_squares = their_grid_margin * scaling_factor
+                square_size = scaling_factor * 106.25  # Their square size
+                square_distance = grid_margin_and_distance_between_squares + square_size
+                #####
+                # offset * 4 + size
+                background_square_positions.append(
+                    pygame.draw.rect(screen, Color("#cdc1b4"), Rect(x*square_distance+grid_margin_and_distance_between_squares+margin,
+                                                                    y*square_distance+grid_margin_and_distance_between_squares+margin,
+                                                                    square_size, square_size))
+                )
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
