2ee3fa7daad3df35a3dffa491267ef684a64a34d
Adrian Metallinos <39782071+I-code-stuff631@users.noreply.github.com>
2023-02-06 12:59:22 -0500

    Lots of changes

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
-# import sys, pygame
-#
-# pygame.init()
-# size = width, height = 320, 240
-# speed = [2, 2]
-# black = 0, 0, 0
-#
-# screen = pygame.display.set_mode(size)
-# ball = pygame.image.load("intro_ball.gif")
-# ballrect = ball.get_rect()
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT: sys.exit()
-#     ballrect = ballrect.move(speed)
-#     if ballrect.left < 0 or ballrect.right > width:
-#         speed[0] = -speed[0]
-#     if ballrect.top < 0 or ballrect.bottom > height:
-#         speed[1] = -speed[1]
-#     screen.fill(black)
-#     screen.blit(ball, ballrect)
-#     pygame.display.flip()
+import random
+
 import pygame
 from pygame.locals import *  # The "Prelude"
 from enum import Enum, auto
 import random as rand
-from typing import final, Final
+from typing import Final  # , final
+
+
+class BackgroundTile:
+    color = Color("#cdc1b4")
+
+    def __init__(self, rect: Rect):
+        self.rect = rect
+
+    def draw(self, screen, border_radius):
+        pygame.draw.rect(screen, self.color, self.rect, border_radius = border_radius)
 
 
 class Direction(Enum):
@@ -33,33 +24,28 @@ class Direction(Enum):
     RIGHT = auto()
 
 
-class Tile:
-    """2048 Tile"""
-    time_to_push_destination_in_mills = 100
+class ForegroundTile:
+    # The time it takes for a tile to slide from one end of the grid to the other (the longest slide time possible)
+    longest_slide_time_in_mills: Final = 100  # For config
+    __instances = []
 
-    @classmethod
-    def init(cls, tile_grid, square_rect_grid, border_radius, /):  # Stop positions
-        """Run this before using this class or creating an instance of it"""
-        cls.tile_grid = tile_grid
-        cls.square_rect_grid = square_rect_grid
-        cls.border_radius = border_radius
-
-    def __init__(self, x, y: (int, int)):
-        self._number = 2
+    def __init__(self, rect: Rect, *, grid_position: (int, int)):
+        self.value = 2
         self._moving = None
-        self._rect = self.square_rect_grid[x][y].copy()
-        self._grid_position = (x, y)
+        self._rect = rect.copy()
+        self._grid_position = grid_position[0], grid_position[1]
 
     def push(self, direction: Direction):
         self._moving = direction
+        self._grid_position = (-1, -1)
 
-    def _move(self):
+    def _move(self, background_tile_grid: list[list[BackgroundTile]], slide_speed):
         match self._moving:
             case Direction.UP:
-                self._rect.move(10, 1)
-                pass
+                self._rect.move(0, -self._move_speed).top > self._grid_position
+                self._rect.move_ip(0, -self._move_speed)
             case Direction.DOWN:
-                pass
+                self._rect.move_ip(0, self._move_speed)
             case Direction.LEFT:
                 pass
             case Direction.RIGHT:
@@ -68,30 +54,27 @@ class Tile:
                 # Not moving
                 pass
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, Color(0, 0, 0), self._rect, border_radius=self.border_radius)
+    def draw(self, screen, border_radius):
+        pygame.draw.rect(screen, Color(0, 0, 0), self._rect, border_radius=border_radius)
 
-    def update(self, screen):
-        self._move()
-        self.draw(screen)
+    def update(self, screen, border_radius, background_tile_grid, slide_speed):
+        self._move(background_tile_grid, slide_speed)
+        self.draw(screen, border_radius)
 
-    # @classmethod
-    # def push_all(cls, direction: Direction):
-    #     cls.lambda_all(lambda tile: tile.push(direction))
-
-    @classmethod
-    def for_each(cls, lamb):
-        # noinspection PyUnresolvedReferences
-        for e in cls.tile_grid:
-            for tile in e:
-                if tile is not None:
-                    lamb(tile)
-
-    # def increment_number(self):
-    #     self._number *= 2
+    @staticmethod
+    def add_one(foreground_tile_grid: list[list], background_tile_grid: list[list[BackgroundTile]]):
+        free_tile_grid_positions = []
+        for (x, e) in enumerate(foreground_tile_grid):
+            for (y, v) in enumerate(e):
+                if v is None:
+                    free_tile_grid_positions.append((x, y))
+        x, y = random.choice(free_tile_grid_positions)
+        # noinspection PyTypeChecker
+        foreground_tile_grid[x][y] = ForegroundTile(background_tile_grid[x][y].rect, grid_position=(x, y))
 
 
-def main(screen_size):  # Treat screen_size as final unless you MUST do otherwise (trust me)
+def main(screen_size, frame_rate):  # Treat screen_size as final unless you MUST do otherwise (trust me)
+    """Make this for initlization code?"""
     pygame.init()
     screen: pygame.Surface = pygame.display.set_mode((screen_size, screen_size))
     screen.fill(Color("#faf8ef"))
@@ -107,67 +90,63 @@ def main(screen_size):  # Treat screen_size as final unless you MUST do otherwis
     ##########################
     their_big_squares_border_radius = 6
     big_square_border_radius = round(scaling_factor * their_big_squares_border_radius)
-    # Their grid margin is the same as the distance between their squares
-    grid_margin = distance_between_squares = scaling_factor * 15  # Their grid margin
+    # Their grid margin is the same as the distance between their tiles
+    grid_margin = distance_between_tiles = scaling_factor * 15  # Their grid margin
     grid_margin += big_square_margin
-    square_size = scaling_factor * 107
-    square_distance = distance_between_squares + square_size
-    their_square_border_radius = 3  # For their background and forground squares
-    square_border_radius = round(scaling_factor * their_square_border_radius)
+    tile_size = scaling_factor * 107
+    tile_distance = distance_between_tiles + tile_size
+    their_tile_border_radius = 3
+    tile_border_radius = round(scaling_factor * their_tile_border_radius)
     # noinspection PyTypeChecker
-    square_rect_grid: Final[list[list[Rect]]] = [
-        [None, None, None, None],
-        [None, None, None, None],
-        [None, None, None, None],
-        [None, None, None, None]
-    ]  # This may also double as stop positions later (that is why it is not just a list)
-    for x in range(4):
-        for y in range(4):
-            square_rect_grid[x][y] =\
-                Rect(x * square_distance + grid_margin,
-                     y * square_distance + grid_margin,
-                     square_size, square_size)
-    tile_grid: list[list[Tile | None]] = [
+    background_tile_grid: list[list[BackgroundTile]] = [
         [None, None, None, None],
         [None, None, None, None],
         [None, None, None, None],
         [None, None, None, None]
     ]
-    Tile.init(tile_grid, square_rect_grid, square_border_radius)
+    for x in range(4):
+        for y in range(4):
+            background_tile_grid[x][y] = BackgroundTile(Rect(
+                x * tile_distance + grid_margin,
+                y * tile_distance + grid_margin,
+                tile_size, tile_size))
+    foreground_tile_grid: list[list[ForegroundTile | None]] = [
+        [None, None, None, None],
+        [None, None, None, None],
+        [None, None, None, None],
+        [None, None, None, None]
+    ]
     for _ in range(2):  # Two starting tiles
-        x, y = rand.randrange(4), rand.randrange(4)
-        tile_grid[x][y] = Tile(x, y)
+        ForegroundTile.add_one(foreground_tile_grid, background_tile_grid)
+    foreground_tile_slide_speed = (background_tile_grid[0][0].rect.left - background_tile_grid[3][0].rect.right) / (
+            (frame_rate / 1000) * ForegroundTile.longest_slide_time_in_mills
+    )
     while True:
         pygame.draw.rect(screen, Color("#bbada0"), big_square_rect, border_radius=big_square_border_radius)
-        for e in square_rect_grid:
-            for rect in e:
-                pygame.draw.rect(screen, Color("#cdc1b4"), rect, border_radius=square_border_radius)
-        Tile.for_each(lambda tile: tile.update(screen))
-        # for e in tile_grid:
-        #     for tile in e:
-        #         if tile is not None:
-        #             tile.update(screen)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-            elif event.type == KEYDOWN:
-                if event.key == K_UP or K_w:
-                    Tile.for_each(lambda tile: tile.push(Direction.UP))
-                    # Tile.push_all(Direction.UP)
-                elif event.key == K_DOWN or K_s:
-                    Tile.for_each(lambda tile: tile.push(Direction.DOWN))
-                    # Tile.push_all(Direction.DOWN)
-                elif event.key == K_LEFT or K_a:
-                    Tile.for_each(lambda tile: tile.push(Direction.LEFT))
-                    # Tile.push_all(Direction.LEFT)
-                elif event.key == K_RIGHT or K_a:
-                    Tile.for_each(lambda tile: tile.push(Direction.RIGHT))
-                    # Tile.push_all(Direction.RIGHT)
+        for e in background_tile_grid:
+            for bg_tile in e:
+                bg_tile.draw(screen, tile_border_radius)
+        for e in foreground_tile_grid:
+            for v in e:
+                if v is not None:
+                    v.update(screen, tile_border_radius, background_tile_grid, foreground_tile_slide_speed)
+        # for event in pygame.event.get():
+        #     if event.type == QUIT:
+        #         return
+        #     elif event.type == KEYDOWN:
+        #         if event.key == K_UP or K_w:
+        #             pass
+        #         elif event.key == K_DOWN or K_s:
+        #             pass
+        #         elif event.key == K_LEFT or K_a:
+        #             pass
+        #         elif event.key == K_RIGHT or K_a:
+        #             pass
         pygame.display.flip()
 
 
 if __name__ == '__main__':
-    main(600)
+    main(600, 10)
 
 # Old test
 # surf = pygame.Surface((50, 50), flags=SRCALPHA)
